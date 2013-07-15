9454d62363cfb0335dcf546c6640be003829236e
Jorge Mayo <jmayomartin@gmail.com>
2013-07-15 23:45:52 +0200

    splitter refactorized a bit

--- splitter.rb
+++ splitter.rb
@@ -6,37 +6,19 @@ class Splitter
   end
   
   def split(filename_sufix = nil)
-  
-    begin
-      myfile = File.open(@filename, 'r')
-      part = 0
-      result = []
-      next_line = myfile.readline
-      result[part] = next_line
-      lines_read = 1
-      while !myfile.eof
-        while lines_read < @lines_by_part
-          next_line = myfile.readline
-          result[part] = result[part] + next_line
-          lines_read += 1
-        end
+    part, lines_read = -1, 1
+    result = []
+    IO.foreach(@filename) {|next_line|
+      if ( @lines_by_part == 1 or (lines_read%@lines_by_part == 1) )
         part += 1
-        next_line = myfile.readline
         result[part] = next_line
-        lines_read = 1
+      else
+        result[part] += next_line
       end
-    #: end of file reached>
-    rescue EOFError => e
-      #$stdout.print "end of file reached: " + e
-      #$stderr.print "end of file reached: " + $!
-      #debugger
-      myfile.close
-    end
+      lines_read += 1
+    }
     
     if filename_sufix
-    #debugger
-      #filename_without_extension = @filename.split(/\.\w+$/)[0]
-      #filename_extension = @filename.split(/\.\w+$/)[1]
       i = 0
       result.each do |part|
         f = File.new(@filename+"."+filename_sufix+i.to_s,"w+")
